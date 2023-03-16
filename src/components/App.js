b07f4321a78847e1c0536b059bd28af9e8f5f82c
Timothy <sanohoang@gmail.com>
2023-03-16 15:11:37 -0500

    completed hooks forms lab

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,15 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  const handleAddItem = (newItem) => {
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleAddItem} />
     </div>
   );
 }
